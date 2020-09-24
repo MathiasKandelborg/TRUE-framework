d27ea8d2de60eba45d9c33ed89ba08d7e724c16d
Mathias Wøbbe <mathias@kandelborg.dk>
2020-09-24 14:58:20 +0200

    refactor(types): refactor page props type

---
+++ typescript/PageProps.d.ts
@@ -0,0 +1,16 @@
+export interface PageProps {
+  config: {
+    title: string
+    url: string
+    logo: {
+      asset: { extension: string; url: string }
+    }
+    mainNavigation: [{ slug: { _type: string; current: string } }]
+    footerNavigation: [{ slug: string }]
+  }
+  allRoutes: Array<{
+    route: string
+    as: string
+  }>
+  preview: boolean
+}
