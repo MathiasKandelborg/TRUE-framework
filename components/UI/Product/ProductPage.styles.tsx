c1eeea615a79a24003cb08c4eba077737141b4dc
Mathias Wøbbe <mathias@kandelborg.dk>
2020-09-26 22:33:21 +0200

    feat(product): add products UI & functionality 🥳

---
+++ components/UI/Product/ProductPage.styles.tsx
@@ -0,0 +1,11 @@
+import * as MUI from '@material-ui/core'
+
+const singleProductPageStyles = MUI.makeStyles((theme: MUI.Theme) =>
+  MUI.createStyles({
+    paper: {
+      padding: theme.spacing(2)
+    }
+  })
+)
+
+export default singleProductPageStyles
