df19e3f6fc6b783c9c85e89b08ad8c6add69fc3e
Mathias Wøbbe <mathias@kandelborg.dk>
2020-09-30 11:19:38 +0200

    feat(components): add titledivider component

---
+++ components/UI/TitleDivider.tsx
@@ -0,0 +1,10 @@
+import * as MUI from '@material-ui/core'
+
+const TitleDivider: React.FC = () => (
+  <MUI.Grid item xs={12}>
+    <MUI.Divider variant="middle" light />
+    <br />
+  </MUI.Grid>
+)
+
+export default TitleDivider
