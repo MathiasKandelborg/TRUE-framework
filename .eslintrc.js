e5b8aa7eeff293df903b034fbfc1de1adbffbf10
Mathias Wøbbe <mathias@kandelborg.dk>
2020-09-30 19:17:26 +0200

    feat(yarn): upgrade to yarn 2
    BREAKING CHANGE: Upgrade to yarn 2 requires using the monorepo to install everything

--- .eslintrc.js
+++ .eslintrc.js
@@ -129,7 +129,7 @@ module.exports = {
         },
         'import/resolver': {
           typescript: {
-            alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+            alwaysTryTypes: false, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
             project: 'tsconfig.json'
           }
         }
