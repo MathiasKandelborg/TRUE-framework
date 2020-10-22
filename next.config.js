4137d6555156441bd14d37e45809d6d870794e7c
Mathias Wøbbe <mathias@kandelborg.dk>
2020-10-22 16:15:02 +0200

    fix: reverting versions didn't do much

--- next.config.js
+++ next.config.js
@@ -20,6 +20,7 @@ module.exports = withPWA({
   reactStrictMode: true,
   trailingSlash: false,
   poweredByHeader: false,
+  target: 'serverless',
   webpack: (
     config /* ,{ buildId, dev, isServer, defaultLoaders, webpack } */
   ) =>
