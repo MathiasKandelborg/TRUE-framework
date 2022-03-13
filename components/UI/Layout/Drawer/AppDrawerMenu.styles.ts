71040fb618085f7dcd82b73b5324037ce70f1ec9
Mathias Wøbbe <mathias@kandelborg.dk>
2022-03-13 16:23:42 +0100

    feat(mui): update mui styles
    First step toward upgrading the entire codebase to latest versions

--- components/UI/Layout/Drawer/AppDrawerMenu.styles.ts
+++
@@ -1,9 +0,0 @@
-import * as MUI from '@material-ui/core'
-
-const appDrawerMenuStyles = MUI.makeStyles((theme: MUI.Theme) =>
-  MUI.createStyles({
-    toolbar: theme.mixins.toolbar
-  })
-)
-
-export default appDrawerMenuStyles
